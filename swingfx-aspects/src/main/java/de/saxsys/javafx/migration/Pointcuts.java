6f2f2f686e11d4ac2fcce940c536abc558cb10ef
Alexander Casall <alexander.casall@saxsys.de>
2016-12-02 16:41:33 +0100

    deleted uneeded file

--- swingfx-aspects/src/main/java/de/saxsys/javafx/migration/Pointcuts.java
+++
@@ -1,8 +0,0 @@
-package de.saxsys.javafx.migration;
-
-public class Pointcuts {
-	public static final String SWINGNODE_SETCONTENT = "call (* javafx.embed.swing.SwingNode.setContent(..))";
-	public static final String JFXPANEL_INIT = "call (javafx.embed.swing.JFXPanel*+.new(..))";
-	public static final String JFXPANEL_ANYMETHOD = "call (* javafx.embed.swing.JFXPanel*+.*(..))";
-	
-}
